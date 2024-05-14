26629e4f7fd18e72388bae091d498cc8a5900f1e
Devishraq <devishraq@gmail.com>
2024-05-14 17:02:28 +0600

    init

---
+++ dist/App.js
@@ -0,0 +1,6 @@
+const string = "hello, World!";
+const App = ()=>{
+    return /*#__PURE__*/ shineElement(wrapper, null, /*#__PURE__*/ shineElement("div", null, /*#__PURE__*/ shineElement("h2", null, "Hello, World!"), /*#__PURE__*/ shineElement("p", null, 120 - 23 / 23)));
+};
+export default App;
+
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const string = "hello, World!"
+const App = ()=>{
+    return (
+        <>
+            <div>
+                <h2>Hello, World!</h2>
+                <p>{120-23/23}</p>
+            </div>
+        </>
+    )
+}
+
+export default App;
