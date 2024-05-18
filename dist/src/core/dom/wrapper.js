726903f1d4ae15cbf390aba0201f4702fd722534
Devishraq <devishraq@gmail.com>
2024-05-18 12:02:23 +0600

    wrapper/fragment soruce code edited : and make the thing much more easier

--- dist/src/core/dom/wrapper.js
+++ dist/src/core/dom/wrapper.js
@@ -1,7 +1,16 @@
-export const wrapper = (...childrens)=>{
+/**
+ * Wraps multiple *Olka* elements into a single DocumentFragment.
+ *
+ * @param {...Node} childrens - The elements to wrap.
+ * @returns {DocumentFragment} The newly created DocumentFragment containing all the elements.
+ */ export const wrapper = (...childrens)=>{
+    // Create a new document fragment.
     const fragment = document.createDocumentFragment();
+    // Iterate over each element.
     childrens.forEach((node)=>{
-        fragment.appendChild(node);
+        // Append the element to the document fragment.
+        console.log(node);
     });
+    // Return the document fragment containing all the elements.
     return fragment;
 };
