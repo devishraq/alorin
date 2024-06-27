4e5e878880c2f71558b8385911b9e7c8f05e9bf0
Devishraq <devishraq@gmail.com>
2024-06-27 23:26:43 +0600

    Refactored createSignal.js for better readability and performance

--- src/core/reactivity/createSignal.js
+++ src/core/reactivity/createSignal.js
@@ -4,29 +4,21 @@ const createSignal = (initialValue) => {
 	let value = initialValue;
 	const subscribers = new Set();
 
+	const notifySubscribers = () => {
+		subscribers.forEach((subscriber) => subscriber());
+	};
+
 	const getValue = () => {
 		if (currentComputation) {
 			subscribers.add(currentComputation);
 		}
-
 		return value;
 	};
 
 	const setValue = (newValue) => {
-		if (typeof newValue === "function") {
-			value = newValue(value);
-			// console.log(value);
-		} else {
-			value = newValue;
-		}
-		notifySubscribers()
+		value = newValue;
+		notifySubscribers();
 	};
-
-	const notifySubscribers = () => {
-		subscribers.forEach((subscriber) => subscriber());
-	}
-	// isSignal is a custom Value which indicates whether it is signal node or a function
-	getValue.isSignal = true;
 
 	return [getValue, setValue];
 };
