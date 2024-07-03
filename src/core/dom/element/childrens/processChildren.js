7d20cd33d5d3412f6313a07a5867bd883b41f376
Devishraq <devishraq@gmail.com>
2024-07-04 00:08:46 +0600

    fix: performace optimized! appending child & node was to the fragment was unoptimized due its illegal usage..as well as better error handling has been added including better null & undefined handler. as well as the complexity of the code has been diminished due to some weird removal of condition checking!

--- src/core/dom/element/childrens/processChildren.js
+++ src/core/dom/element/childrens/processChildren.js
@@ -1,25 +1,20 @@
 import { signalHandler } from "../../signal/index.js";
 import { processNestedChildren } from "./processNestedChildren.js";
 
-export const processChildrens = (childrens, fragment, childNode) => {
+export const processChildrens = (childrens, fragment) => {
     childrens.forEach((node) => {
-        if (Array.isArray(node)) {
-            processNestedChildren(node, fragment);
-        } else if (node == null) {
-            return;
-        } else if (node instanceof Node) {
-            fragment.appendChild(node);
-        } else if (typeof node === "function") {
-            node.isSignal
+
+        let childNode = null;
+        if (node === null || node === undefined) return;
+
+        if (Array.isArray(node)) processNestedChildren(node, fragment);
+        else if (node instanceof Node) childNode = node;
+        else if (typeof node === "function")
+            childNode = node.isSignal
                 ? signalHandler(node, fragment)
-                : fragment.appendChild(node(elementProps));
-        } else {
-            if (node !== undefined) {
-                childNode = document.createTextNode(node);
-                fragment.appendChild(childNode);
-            } else {
-                return;
-            }
-        }
+                : (childNode = node(elementProps));
+        else childNode = document.createTextNode(String(node));
+
+        if (childNode) fragment.appendChild(childNode);
     });
 };
--- src/core/dom/element/childrens/processNestedChildren.js
+++ src/core/dom/element/childrens/processNestedChildren.js
@@ -1,18 +1,18 @@
-
-
-export const processNestedChildren = (child, childNode) => {
+export const processNestedChildren = (child, fragment) => {
     child.forEach((child) => {
+        if (child === null || child === undefined) return;
+
+        let childToAppend = null;
+
         if (child instanceof Node) {
-            fragment.appendChild(child);
+            childToAppend = child;
         } else if (typeof child === "function") {
             const childElement = child(elementProps);
-            if (childElement instanceof Node) {
-                fragment.appendChild(childElement);
-            }
+            if (childElement instanceof Node) childToAppend = childElement;
         } else {
-            childNode = document.createTextNode(child);
-            fragment.appendChild(childNode);
+            childToAppend = document.createTextNode(String(child));
         }
+
+        if (childToAppend) fragment.appendChild(childToAppend);
     });
 };
-
