ee4f3a8341224b87e0ca1cd142015224ad029140
Devishraq <devishraq@gmail.com>
2024-07-04 23:29:34 +0600

    breaking changes: some test result says that nested childrebn is only handles the array as children moreover the mapping things, so that this file don't need to need any type of elment handling, rather it wil be enough if it is able to handle the array data strcuture as children...
     my idea was createElement(tag, props, [...childrens]) .. but, I've made something like createElement(tag, props, ...childrens) .. so, I think I don't need to handle the childrens as an array. I can handle them as *arguments.

--- src/core/dom/element/childrens/processNestedChildren.js
+++ src/core/dom/element/childrens/processNestedChildren.js
@@ -1,17 +1,29 @@
+// import signalHandler
+import { signalHandler } from "../../signal/";
+
 export const processNestedChildren = (child, fragment) => {
+
+
     child.forEach((child) => {
         if (child === null || child === undefined) return;
 
         let childToAppend = null;
 
         if (child instanceof Node) {
+            console.log(child)
             childToAppend = child;
-        } else if (typeof child === "function") {
-            const childElement = child(elementProps);
-            if (childElement instanceof Node) childToAppend = childElement;
-        } else {
-            childToAppend = document.createTextNode(String(child));
         }
+
+        // All of the bellow commented code are looks like has no contribution in the final output, so I commented them out. my idea was createElement(tag, props, [...childrens]) .. but, I've made something like createElement(tag, props, ...childrens) .. so, I think I don't need to handle the childrens as an array. I can handle them as *arguments. they will be removed in future commits.
+
+        // } else if (typeof child === "function") {
+        //     console.log('Hello from condition block2');
+        //     const childElement = child(elementProps);
+        //     if (childElement instanceof Node) childToAppend = childElement;
+        // } else {
+        //     console.log('Hello from condition block3');
+        //     childToAppend = document.createTextNode(String(child));
+        // }         
 
         if (childToAppend) fragment.appendChild(childToAppend);
     });
