ec3764b009eceb4a3dde43d0646a467a92437b0d
Devishraq <devishraq@gmail.com>
2024-07-04 23:36:23 +0600

    refactor: App.js & Button has been updated for better test cases.. though there are not actual test files more-over examples but helps to check the effieciency of the code..........
    there is a plan to add examples in ./example and write some real test cases using jest or mocha

--- src/test/App.js
+++ src/test/App.js
@@ -1,52 +1,74 @@
 import { olka, createSignal, createEffect } from "../core";
 
+import Button from "./Button";
+
+
 const App = () => {
-	const [todos, setTodos] = createSignal(['todo 1', 'todo 2', 'todo 3', 'todo 4', 'todo 5']);
+	const [todos, setTodos] = createSignal(
+		[
+			"todo 1",
+			"todo 2",
+			"todo 3",
+			"todo 4",
+			"todo 5",
+		]
+	);
+	const [counter, setCounter] = createSignal(0)
 	const [newTodo, setNewTodo] = createSignal("");
 
 	const addTodo = () => {
-		setTodos([...todos(), newTodo()]);
+		setTodos(prevState => [...prevState(), newTodo()]);
 		setNewTodo("");
 	};
 
 	const inputHandler = (e) => {
 		setNewTodo(e.target.value);
-	}
+		setCounter(e.target.value)
+
+	};
+
 	createEffect(() => {
 		console.log("Todos:", todos());
 	});
 
-
-	const Todo = () => {
-		return (<ul>
-			{todos().map((todo, index) => (
-				<>
-					<li key={index}>{todo}</li>
-				</>
-			))}
-		</ul>
-		)
-	}
-
 	return (
 		<div>
-			<button style={{
-				'position': 'absoulute',
-				'right': '0',
-				'top': '0',
-				'zIndex': '100'
-			}} onClick={Todo}>re-render the ul</button>
 			<input
 				type="text"
 				value={newTodo()}
 				onInput={inputHandler}
 			/>
 			<button onClick={addTodo}>Add Todo</button>
-			<Todo />
+			<ul>
+				{todos().map((todo, index) => (
+					<>
+						<div>{counter}
+							<li key={index}>{todo}</li>
+						</div>
+						<Button>Click Me!</Button>
+					</>
+				))}
+			</ul>
+
+			<ol>
+				<li>
+					<a target="_blank" href="google.com">
+						<span>Hey Howdy1</span>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="google.com">
+						<span>Hey Howdy2</span>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="google.com">
+						<span>Hey Howdy3</span>
+					</a>
+				</li>
+			</ol>
 		</div>
 	);
-}
-
-
+};
 
 export default App;
