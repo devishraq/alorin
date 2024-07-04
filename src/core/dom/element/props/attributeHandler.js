3b0e27a3ae2770930322f6c57a691801740d0ede
Devishraq <devishraq@gmail.com>
2024-07-04 15:02:48 +0600

    fix: the event props were directly injected to the html make it dirty...so, a bit sanitization has been done....and the conditions has been re-visited and handled perfectly the event & children even null and undefined attribute!

--- src/core/dom/element/props/attributeHandler.js
+++ src/core/dom/element/props/attributeHandler.js
@@ -2,15 +2,11 @@ import { styleHandler } from "./";
 
 export const attributeHandler = (elementProps, element) => {
     for (const attribute in elementProps) {
-        if (attribute == "style") {
-            styleHandler(elementProps, element);
-        } else {
-            attribute === "children"
-                ? null
-                : element.setAttribute(
-                    attribute,
-                    elementProps[attribute]
-                );
-        }
+        console.log(attribute.slice(2).toLowerCase());
+        if (attribute == null || attribute == undefined) null;
+        if (attribute == "style") styleHandler(elementProps, element);
+        else if (attribute.startsWith('on') || attribute == "children") null;
+        else element.setAttribute(attribute, elementProps[attribute])
     }
-};
+}
+
