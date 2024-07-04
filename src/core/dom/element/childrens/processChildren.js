3eef3957983b0d1c32613d3018e9b34c46b4bdbf
Devishraq <devishraq@gmail.com>
2024-07-04 23:34:05 +0600

    refactor: removed loggs which was for some testing like mutation detection in object data ty[e(array, object)

--- src/core/dom/element/childrens/processChildren.js
+++ src/core/dom/element/childrens/processChildren.js
@@ -6,15 +6,15 @@ export const processChildrens = (childrens, fragment) => {
 
         let childNode = null;
         if (node === null || node === undefined) return;
-        console.log('signal', node.isSignal);
+
         if (Array.isArray(node)) processNestedChildren(node, fragment);
+
         else if (node instanceof Node) childNode = node;
+
         else if (typeof node === "function") {
             if (node.isSignal) {
-                console.log("processChildrens -> node", node());
                 childNode = signalHandler(node, fragment);
             } else {
-                console.log("processChildrens -> node", node());
                 childNode = node(elementProps);
             }
         }
