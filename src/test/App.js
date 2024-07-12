bc9fb1dca84a7351899e7dbf38abc47d04a99b6e
Devishraq <devishraq@gmail.com>
2024-07-12 19:08:07 +0600

    chore: App.js has been updated for testing capabilities of Display Component.....

--- src/test/App.js
+++ src/test/App.js
@@ -1,49 +1,67 @@
-import { alorin, createSignal, createEffect, Show } from "../core";
+import { alorin, createSignal, createEffect, Display } from "../core";
+
+const Loading = () => {
+	const [loading, setLoading] = createSignal(true);
+	createEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	});
+
+	return (
+		<Display
+			// @ts-ignore
+			condition={loading}
+		>
+			<p>Loading...</p>
+		</Display>
+	);
+};
 
 const App = () => (
 	<div>
-		<h1>Show Component Test</h1>
-		<Show condition={true}>
+		<h1>Display Component Test</h1>
+		<Display delay={500} condition={true}>
 			<p>This should be displayed</p>
-		</Show>
-		<Show condition={false}>
+		</Display>
+		<Display condition={false}>
 			<p>This should not be displayed</p>
-		</Show>
-		<Show condition={1 > 2}>
+		</Display>
+		<Display condition={1 > 2} fallback={<Loading/>}>
 			<p>1 is greater than 2 (should not be displayed)</p>
-		</Show>
-		<Show condition={5 === 5}>
+		</Display>
+		<Display condition={5 === 5}>
 			<p>5 equals 5 (should be displayed)</p>
-		</Show>
-		<Show condition={null}>
+		</Display>
+		<Display condition={null}>
 			<p>This should not be displayed</p>
-		</Show>
-		<Show condition={true}>
+		</Display>
+		<Display condition={true}>
 			<div>
-				<h2>Nested Show Component</h2>
-				<Show condition={false}>
+				<h2>Nested Display Component</h2>
+				<Display condition={false}>
 					<p>
 						This nested content should not
 						be displayed
 					</p>
-				</Show>
-				<Show condition={true}>
+				</Display>
+				<Display condition={true}>
 					<p>
 						This nested content should be
 						displayed
 					</p>
-				</Show>
+				</Display>
 			</div>
-		</Show>
-		<Show condition={true}>
+		</Display>
+		<Display condition={true}>
 			<ul>
 				{[1, 2, 3, 4, 5].map((num) => (
-					<Show condition={num % 2 === 0}>
+					<Display condition={num % 2 === 0}>
 						<li key={num}>{num} is even</li>
-					</Show>
+					</Display>
 				))}
 			</ul>
-		</Show>
+		</Display>
 	</div>
 );
 
