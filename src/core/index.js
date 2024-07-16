30b32bc835184e8171436a3d80999608bd9072d9
Devishraq <devishraq@gmail.com>
2024-07-17 00:04:59 +0600

    feat: Add render function for DOM element rendering

--- src/core/index.js
+++ src/core/index.js
@@ -1,4 +1,5 @@
 export * as alorin from "./dom";
+export { render } from "./render";
 export { createStyle } from "./dynamicStyle";
 export { createSignal, createEffect } from "./reactivity";
-export { Display } from './widget'
+export { Display } from "./widget";
---
+++ src/core/render/render.js
@@ -0,0 +1,9 @@
+export const render = (element, container) => {
+	// Cleaning the container
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+
+	// Append the new element to the container
+	container.appendChild(element);
+};
