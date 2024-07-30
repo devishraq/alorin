c487eded9387325bdc0f518757f029849d6084f7
Devishraq <devishraq@gmail.com>
2024-07-30 22:40:32 +0600

    ✨ refactor: merged `generateClass` fn to the `createStyle` fn for removing over splitting
    The code changes in this commit refactor the dynamic styling functionality by merging generateClass to the createStyle. This change improves the efficiency and readability of the code and removed over code splitting

--- src/core/dynamic-style/create-style.js
+++ src/core/dynamic-style/create-style.js
@@ -1,15 +1,11 @@
 import { createElement } from "../dom";
-import { generateClass } from "./generate-class";
 import { insertRule } from "./insert-rule";
+import { nanoid } from "nanoid";
 
-// createStyle function to createStyling according to tag & styles, className is auto-genrated from @generateClass function.
 export const createStyle = (tag) => (styles) => {
 	// generator function
-	const className = generateClass(tag);
 
-	// CSS styles to pushed to css stylesheet
-	insertRule(className, styles.join(""));
-
-	// Returning newly created element with the help of @createElement class
-	return (props) => createElement(tag, { ...props, class: className });
+	const cN = `$__${tag}__${nanoid(5)}`;
+	insertRule(cN, styles.join(""));
+	return (props) => createElement(tag, { ...props, class: cN });
 };
