712de3ca729e9c900b9ae8b9473c29b0f7f0a54d
Devishraq <devishraq@gmail.com>
2024-08-01 17:13:58 +0600

    💫 add: new utility fn `isSVG` introduced
    This utility fn will help to determine any of copiled jsx has been a svg or not!

--- src/utils/checkers.js
+++ src/utils/checkers.js
@@ -1,10 +1,10 @@
 // isArr(node) - it's the array checkers
 // isNUB(node) - N-null, U-undefined, B-Boolean. it's the NUB checkers
 
-export const isArr = (a) => Array.isArray(a);
-export const isNUB = (h) =>
-	h === null || h === undefined || h === false || h === true;
-export const isNode = (n) => n instanceof Node;
-export const isFunc = (f) => typeof f === "function";
-export const isStr = (s) => typeof s === "string";
-export const isObj = (n) => typeof n === "object";
+export const isArr = a => Array.isArray(a);
+export const isNUB = h => h === null || h === undefined || h === false || h === true;
+export const isNode = n => n instanceof Node;
+export const isSVG = s => s instanceof SVGElement;
+export const isFunc = f => typeof f === "function";
+export const isStr = s => typeof s === "string";
+export const isObj = n => typeof n === "object";
