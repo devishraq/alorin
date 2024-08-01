f03c12aad138c85951904b5bfb4227709f6420ea
Devishraq <devishraq@gmail.com>
2024-08-01 17:02:09 +0600

    ✨ refactor: Enhanced Condition checking by adding `NUB` checking directly as case statement in switch
    - previously, NUB were checing at first with if statements....now, it has been added to switch statements which definetly enhanced readbility!

--- src/core/dom/element/childrens/children-processor.js
+++ src/core/dom/element/childrens/children-processor.js
@@ -1,37 +1,32 @@
-import { signalHandler } from "../../signal/index.js";
-import { createEffect } from "../../../reactivity/signal/index.js";
-import { processNestedChildren } from "./";
-import { isArr, isFunc, isNode, isNUB, newTextNode } from "../../../../utils";
-import { childAppender } from "../../../../utils/creators.js";
+import { signalHandler } from "../../signal";
+ import { processNestedChildren } from "./";
+import { isArr, isFunc, isNode, isNUB, newTextNode, childAppender } from "../../../../utils";
+import "../../../../utils/loopers.js";
 
 export const processChildrens = (childrens, fragment) => {
-	childrens.forEach((node) => {
-		let childToAppend = null;
+  childrens.forEach(node => {
+    let childToAppend = null;
 
-		// If node is null or undefined or bool, f**k it
-		if (isNUB(node)) return;
+    switch (true) {
+      case isNUB(node):
+        return;
+      case isArr(node):
+        processNestedChildren(node, fragment);
+        break;
+      case isNode(node):
+        childToAppend = node;
+        break;
+      case isFunc(node):
+        if (node.isSignal) childToAppend = signalHandler(node, fragment);
+        else childToAppend = node();
+        console.log("me");
+        break;
 
-		switch (true) {
-			case isArr(node):
-				processNestedChildren(node, fragment);
-				break;
+      default:
+        childToAppend = newTextNode(node);
+        break;
+    }
 
-			case isNode(node):
-				childToAppend = node;
-				break;
-
-			case isFunc(node):
-				if (node.isSignal)
-					childToAppend = signalHandler(node, fragment);
-				else childToAppend = node();
-				break;
-
-			default:
-				childToAppend = newTextNode(node);
-				break;
-		}
-
-		// If childNode is not null or undefined, append it to the fragment
-		if (childToAppend) childAppender(fragment, childToAppend);
-	});
+    if (childToAppend) childAppender(fragment, childToAppend);
+  });
 };
